933b11f6d74b5fd8b82f241f235e3f60d59e9e0e
JakubKwak <kubakwak123@gmail.com>
2020-02-12 19:46:12 +0000

    Added css

--- src/www/campustreks/login.php
+++ src/www/campustreks/login.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link rel="stylesheet" href="/css/stylesheet.css" />
+  <link rel="stylesheet" href="/css/login_stylesheet.css" />
 	<link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
 	<link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
   </head>
@@ -26,7 +27,7 @@
           </div>
           <button class="login-submit" type="submit">Log In</button>
           <br>
-          <?php if (isset($_GET["loginFailed"])) echo "<p>E-mail or password incorrect</p>"; ?>
+          <?php if (isset($_GET["loginFailed"])) echo "<p class='error'>E-mail or password incorrect</p>"; ?>
         </div>
       </form>
     </div>
