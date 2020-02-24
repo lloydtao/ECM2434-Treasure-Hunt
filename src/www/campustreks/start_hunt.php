3ba0493301c89dd9daf12a7f71bc7f6ba176b0a5
marektancak0106 <marek@tancak.com>
2020-02-24 12:51:37 +0000

    Check if game pin exists

--- src/www/campustreks/start_hunt.php
+++ src/www/campustreks/start_hunt.php
@@ -2,19 +2,24 @@
 	function generateGamePin() {
 	    $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 	    $randomPIN = '';
-	    for ($i = 0; $i < 10; $i++) {
+	    
+	    for ($i = 0; $i < 4; $i++) {
 	        $randomPIN .= $characters[rand(0, (strlen($characters)) - 1)];
+	    }
+
+	    $files = scandir('./hunt_sessions');
+	    if (in_array($randomPIN.'.json', $files)){
+	    	$randomPIN = generateGamePin();
 	    }
 	    return $randomPIN;
 	}
 
 	$huntID = $_GET['huntID'];
-	$huntSessionID = 5678;
 	$gamePIN = generateGamePin();
 	$huntSession = array('gamePIN'=> $gamePIN, 'huntID'=>$huntID, 'teams'=>array());
 	$json_data = json_encode($huntSession);
-	file_put_contents('hunt_sessions/' . $huntSessionID . '.json', $json_data);
+	file_put_contents('hunt_sessions/' . $gamePIN . '.json', $json_data);
 
-	header('Location: /hunt_session.php?sessionID=' . $huntSessionID);
+	header('Location: /hunt_session.php?sessionID=' . $gamePIN);
 	die();
 ?>
