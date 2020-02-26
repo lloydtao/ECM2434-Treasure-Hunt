81925417cbb2b32975fbc4f288425fd06163b070
JakubKwak <kubakwak123@gmail.com>
2020-02-26 18:46:37 +0000

    Add basic join functionality to the html form
    added ajax to play.php to make it dynamic
    created joingame.php which handles the form data
    created 2 example jsons with data in them

---
+++ src/www/campustreks/joingame.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Script for handling game join form
+ * @author Jakub Kwak
+ */
+
+/**
+ * Make data safe my stripping slashes, removing whitespace and special chars from string
+ * @param $data
+ * @return string
+ */
+function makeSafe($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+/**
+ * Attempt to find a game session file with given pin, if a file is found, saves the pin to session and returns true.
+ * @param $pin
+ * @return boolean
+ */
+function findGame($pin)
+{
+    $filename = './hunt_sessions/' . $pin . '.json';
+    if (file_exists($filename))
+    {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Attempt to join a game based on POST data. Checks for duplicate player names and saves info to session.
+ */
+function joinGame()
+{
+    if (!isset($_POST['pin']) || !isset($_POST['nickname'])) {
+        echo "form-error";
+        return;
+    }
+    $pin = makeSafe($_POST['pin']);
+    $nickname = makeSafe($_POST['nickname']);
+
+    //if either field is empty, echo error
+    if (empty($pin) || empty($nickname)) {
+        echo "form-error";
+        return;
+    }
+
+    if (findGame($pin))
+    {
+        //read and parse hunt json
+        $filename = './hunt_sessions/' . $pin . '.json';
+        $jsonString = file_get_contents($filename);
+        $parsedJson = json_decode($jsonString, true);
+
+        //Check player list for duplicate names
+        $playerList = $parsedJson["teams"]["team0"]["players"];
+        foreach ($playerList as $player) {
+            if ($player["playername"] == $nickname) {
+                echo "name-error";
+                return;
+            }
+        }
+        //create new player assoc array
+        $player = array("playername"=>$nickname);
+        $playerCount = count($playerList);
+        //insert player into json data
+        $parsedJson["teams"]["team0"]["players"]["player".$playerCount] = $player;
+
+        //update json file
+        $newJson = json_encode($parsedJson);
+        file_put_contents($filename, $newJson);
+
+        //put hunt data in session
+        session_start();
+        $_SESSION['gameID'] = $pin;
+        $_SESSION['nickname'] = $nickname;
+
+        echo "join-success";
+        return;
+    } else {
+        echo "pin-error";
+        return;
+    }
+}
+
+//if form was submitted, try to join game
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    joinGame();
+}
--- src/www/campustreks/play.php
+++ src/www/campustreks/play.php
@@ -1,22 +1,62 @@
 <!DOCTYPE html>
-  <head>
-    <meta charset="UTF-8" />
+<head>
+    <meta name="author" content="James Caddock">
+    <meta name="contributors" content="Jakub Kwak">
+    <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/css/mobile.css" />
-    <link rel="stylesheet" href="/css/stylesheet.css" />
-  </head>
+    <link rel="stylesheet" href="/css/mobile.css"/>
+    <link rel="stylesheet" href="/css/stylesheet.css"/>
 
-  <body>
-    <form action='post.php' method='POST'>
-      <div class='play-box'>
-        <div class='play-content'>
-          <input type='text' name='pin' maxlength='8' size='12' placeholder='Pin'>
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $("#join-form").submit(function (e) {
+                e.preventDefault();
+                $("#pin-error").css("display", "none");
+                $("#name-error").css("display", "none");
+                $("#form-error").css("display", "none");
+                $.ajax({
+                    type: "POST",
+                    url: "joingame.php",
+                    data: $(this).serialize(),
+                    success: function (data) {
+                        if (data === "join-success") {
+                            $("#game-join").css("display", "none");
+                            //display team select
+                        } else if (data === "pin-error") {
+                            $("#pin-error").css("display", "block");
+                        } else if (data === "name-error") {
+                            $("#name-error").css("display", "block");
+                        } else if (data === "form-error") {
+                            $("#form-error").css("display", "block");
+                        }
+                    }
+                });
+            });
+        });
+    </script>
+</head>
+
+<body>
+<div id="game-join">
+    <form id='join-form' method='POST'>
+        <div class='play-box'>
+            <div class='play-content'>
+                <input type='text' name='pin' maxlength='4' size='4' placeholder='Pin'>
+                <p id='pin-error' style="display: none">Game not found</p>
+            </div>
+            <div class='play-content'>
+                <input type='text' name='nickname' maxlength='15' minlength='2' size='18' placeholder='Nickname'>
+                <p id="name-error" style="display: none">Nickname taken</p>
+            </div>
+            <button class='play-submit' name='submit' type='submit'>Play</button>
+            <p id="form-error" style="display: none">Please fill in all fields</p>
         </div>
-        <div class='play-content'>
-          <input type='text' name='nickname' maxlength='15' minlength='2' size='18' placeholder='Nickname'>
-        </div>
-        <button class='play-submit' type='submit'>Play</button>
-      </div>
     </form>
-  </body>
+</div>
+
+<div id="team-select">
+
+</div>
+</body>
 </html>
