e7c5f96408617c63665df7544b5efd330d905f05
LloydTao <LL23@live.co.uk>
2020-03-07 01:58:30 +0000

    Move /www/campustreks to /app

--- src/www/campustreks/templates/head.php
+++
@@ -1,9 +0,0 @@
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-<link rel="icon" type="image/png" href="/img/favicon-32x32.png" sizes="32x32" />
-<link rel="icon" type="image/png" href="/img/favicon-16x16.png" sizes="16x16" />
-<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:300,400,700">
-<link rel="stylesheet" href="fonts/fontawesome-all.min.css">
-<link rel="stylesheet" href="fonts/ionicons.min.css">
-<link rel="stylesheet" href="css/styles.css">
