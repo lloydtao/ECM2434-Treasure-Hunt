029b8e8eec8cde04da5b48fac8c782ed143c2b14
JakubKwak <kubakwak123@gmail.com>
2020-03-07 13:19:14 +0000

    null json write fix

--- src/www/campustreks/api/endsession.php
+++ src/www/campustreks/api/endsession.php
@@ -12,31 +12,32 @@ function endSession()
     $pin = $_SESSION['gameID'];
     $nickname = $_SESSION['nickname'];
 
-    //read and parse hunt json
-    $filename = '../hunt_sessions/' . $pin . '.json';
-    $jsonString = file_get_contents($filename);
-    $parsedJson = json_decode($jsonString, true);
+    if ($pin != null) {
+        //read and parse hunt json
+        $filename = '../hunt_sessions/' . $pin . '.json';
+        $jsonString = file_get_contents($filename);
+        $parsedJson = json_decode($jsonString, true);
 
-    $counter = 0;
-    $oldteam = "";
-    if (isset($_SESSION["teamName"])) {
-        $oldteam = $_SESSION["teamName"];
-    }
-    $playerList = $parsedJson["teams"][$oldteam]["players"];
-    $parsedJson["teams"][$oldteam]["players"] = [];
-    foreach ($playerList as $player) {
-        if (!(strtoupper($player) == strtoupper($nickname))) {
-            array_push($parsedJson["teams"][$oldteam]["players"], $player);
-            $counter += 1;
-        } else {
-            $found = true;
+        $counter = 0;
+        $oldteam = "";
+        if (isset($_SESSION["teamName"])) {
+            $oldteam = $_SESSION["teamName"];
         }
+        $playerList = $parsedJson["teams"][$oldteam]["players"];
+        $parsedJson["teams"][$oldteam]["players"] = [];
+        foreach ($playerList as $player) {
+            if (!(strtoupper($player) == strtoupper($nickname))) {
+                array_push($parsedJson["teams"][$oldteam]["players"], $player);
+                $counter += 1;
+            } else {
+                $found = true;
+            }
+        }
+
+        //update json file
+        $newJson = json_encode($parsedJson);
+        file_put_contents($filename, $newJson);
     }
-
-    //update json file
-    $newJson = json_encode($parsedJson);
-    file_put_contents($filename, $newJson);
-
     
     unset($_SESSION['nickname']);
     unset($_SESSION['teamName']);
