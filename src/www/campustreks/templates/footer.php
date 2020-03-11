b507d269a4217b5064456d906bc52d9679605292
freemanm65 <44925374+freemanm65@users.noreply.github.com>
2020-03-11 14:55:34 +0000

    Revert "links to other sites will now open in new tab"
    This reverts commit 899b23431955ed0c1d8f37ce9dacae51a57c5e62.

--- src/www/campustreks/templates/footer.php
+++ src/www/campustreks/templates/footer.php
@@ -7,9 +7,9 @@
 
 		</div>
         <div class="social-icons">
-			<a href="https://twitter.com/Campus_treks" target="_blank"><i class="fab fa-twitter"></i></a>
-			<a href="https://www.instagram.com/campus_treks/?hl=en" target="_blank"><i class="fab fa-instagram"></i></a>
-			<a href="https://github.com/LloydTao/ECM2434-Treasure-Hunt" target="_blank"><i class="fab fa-github"></i></a>
+			<a href="https://twitter.com/Campus_treks"><i class="fab fa-twitter"></i></a>
+			<a href="https://www.instagram.com/campus_treks/?hl=en"><i class="fab fa-instagram"></i></a>
+			<a href="https://github.com/LloydTao/ECM2434-Treasure-Hunt"><i class="fab fa-github"></i></a>
 		</div>
     </div>
 </footer>
